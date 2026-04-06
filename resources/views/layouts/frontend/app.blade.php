45bbd016ed64530f5462002c2d079ae69d56ef0b
AL Amin <alamin041057@gmail.com>
2026-04-06 11:39:39 +0600

    Meta description

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-    <meta name="description" content='{{ $detail ?? config('setting.detail') }}'>
+    <meta name="description" content='{{ $seo['description'] ?? config('setting.detail') }}'>
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
